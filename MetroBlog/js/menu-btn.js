59dc8f9fa4b5a66c697c0dfbe463368d9a8fbaa3
zenker91 <roman.vyazovyi@gmail.com>
2019-11-17 14:23:18 +0200

    Mobile navigation fixed

--- MetroBlog/js/menu-btn.js
+++ MetroBlog/js/menu-btn.js
@@ -1,18 +1,18 @@
 const menu_btn = document.querySelector('.menu-btn');
-const navigation = document.querySelector('.navigation')
-const nav_items = document.getElementsByClassName('.nav_item')
+const navigation_mobile = document.querySelector('.navigation_mobile');
+const nav_items = document.getElementsByClassName('.nav_item');
 
 
 menu_btn.onclick = (e) => {
   e.preventDefault;
-  if (navigation.style.opacity == "1") {navigation.style.opacity = "0";
+  if (navigation_mobile.style.opacity == "1") {navigation_mobile.style.opacity = "0";
 setTimeout(() => {
-  navigation.style.zIndex = "-1";
+  navigation_mobile.style.zIndex = "-1";
 }, 300)}
-  else {navigation.style.opacity = "1";
-  navigation.style.zIndex = "99";
+  else {navigation_mobile.style.opacity = "1";
+  navigation_mobile.style.zIndex = "99";
   }
 
   menu_btn.classList.toggle("menu-btn_active");
-  navigation.classList.toggle("nav_active");
+  navigation_mobile.classList.toggle("nav_active");
 }
