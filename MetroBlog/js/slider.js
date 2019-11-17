7b34f4f509bb45fc2e381b22e67f8dc298e5342b
zenker91 <roman.vyazovyi@gmail.com>
2019-11-17 13:30:22 +0200

    Rewrited slider

--- MetroBlog/js/slider.js
+++ MetroBlog/js/slider.js
@@ -1,19 +1,37 @@
-let left_button = document.querySelector('.left_button');
-let right_button = document.querySelector('.right_button');
-let slider = document.querySelector('.slider');
-let slider_size = document.getElementsByClassName('slider_item').length - 1 ;
-let slider_frame_width = document.querySelector('.slider_item').clientWidth;
+const left_button = document.querySelector('.left_button');
+const right_button = document.querySelector('.right_button');
+const slider = document.querySelector('.slider');
+const slider_size = document.getElementsByClassName('slider_item').length - 1 ;
+const slider_item = document.querySelector('.slider_item');
+
+let slider_frame_width = slider_item.clientWidth;
 let pos = 0;
+let current_slide = 0;
 
-left_button.onclick = slider_right;
-right_button.onclick = slider_left;
+window.addEventListener('resize', resize);
+left_button.addEventListener('click', slider_right);
+right_button.addEventListener('click', slider_left);
 
+function current_slide_change() {
+  current_slide = (pos-slider_frame_width)/-slider_frame_width;
+}
+
+function resize() {
+  let old_slider_frame_width = slider_frame_width;
+  slider_frame_width = document.querySelector('.slider_item').clientWidth;
+  if (old_slider_frame_width != slider_frame_width) {
+    pos = (1-current_slide)*slider_frame_width;
+    slider_left();
+    slider_right();
+  }
+}
 
 function slider_left() {
   if (pos > -slider_frame_width*slider_size) {
     pos -= slider_frame_width;
     slider.style.left = pos + 'px';
-  } 
+  }
+  current_slide_change();
 }
 
 function slider_right() {
@@ -21,4 +39,5 @@ function slider_right() {
   pos += slider_frame_width;
   slider.style.left = pos + 'px';
   }
+  current_slide_change();
 }
