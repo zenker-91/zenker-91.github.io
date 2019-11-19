d016257e087669e697e87d73ebfe0b5657ecbf54
zenker91 <roman.vyazovyi@gmail.com>
2019-11-19 21:57:21 +0200

    Добавлена адаптивность

---
+++ ServicePage/js/menu-btn.js
@@ -0,0 +1,18 @@
+const menu_btn = document.querySelector('.menu-btn');
+const navigation_mobile = document.querySelector('.navigation_mobile');
+const nav_items = document.getElementsByClassName('.nav_item');
+
+
+menu_btn.onclick = (e) => {
+  e.preventDefault;
+  if (navigation_mobile.style.opacity == "1") {navigation_mobile.style.opacity = "0";
+setTimeout(() => {
+  navigation_mobile.style.zIndex = "-1";
+}, 300)}
+  else {navigation_mobile.style.opacity = "1";
+  navigation_mobile.style.zIndex = "1";
+  }
+
+  menu_btn.classList.toggle("menu-btn_active");
+  navigation_mobile.classList.toggle("nav_active");
+}
